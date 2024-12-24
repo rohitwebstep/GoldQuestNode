f5078a00d04fb472c7093dbfab77d46f5251a7d2
yashika181007 <yashikawebstep@gmail.com>
2024-12-24 12:53:15 +0530

    updated

--- src/models/admin/candidateMasterTrackerModel.js
+++ src/models/admin/candidateMasterTrackerModel.js
@@ -15,7 +15,12 @@ const Customer = {
         return callback(err, null);
       }
 
-      if (filter_status && filter_status !== null && filter_status !== "") {
+      if (
+        filter_status &&
+        filter_status.trim().toLowerCase() &&
+        (filter_status.trim().toLowerCase() === "submitted" ||
+          filter_status.trim().toLowerCase() === "unsubmitted")
+      ) {
         // Query when `filter_status` exists
         const sql = `
           SELECT b.customer_id, 
@@ -23,7 +28,7 @@ const Customer = {
                  b.name AS branch_name, 
                  COUNT(ca.id) AS application_count,
                  MAX(ca.created_at) AS latest_application_date
-          FROM client_applications ca
+          FROM candiate_applications ca
           INNER JOIN branches b ON ca.branch_id = b.id
           WHERE ca.status = ?
           GROUP BY b.customer_id, b.id, b.name
@@ -51,51 +56,64 @@ const Customer = {
 
           const finalSql = `
             WITH BranchesCTE AS (
+                    SELECT 
+                        b.id AS branch_id,
+                        b.customer_id
+                    FROM 
+                        branches b
+                    WHERE 
+                        EXISTS (
+                            SELECT 1 
+                            FROM candidate_applications ca 
+                            WHERE ca.branch_id = b.id
+                        )
+                ),
+                ApplicationCounts AS (
+                    SELECT 
+                        b.customer_id, 
+                        COUNT(ca.id) AS application_count,
+                        MAX(ca.created_at) AS latest_application_date
+                    FROM 
+                        BranchesCTE b
+                    INNER JOIN 
+                        candidate_applications ca ON b.branch_id = ca.branch_id
+                    GROUP BY 
+                        b.customer_id
+                )
                 SELECT 
-                    b.id AS branch_id,
-                    b.customer_id
+                    customers.client_unique_id,
+                    customers.name,
+                    customer_metas.tat_days,
+                    customer_metas.single_point_of_contact,
+                    customers.id AS main_id,
+                    COALESCE(branch_counts.branch_count, 0) AS branch_count,
+                    COALESCE(application_counts.application_count, 0) AS application_count
                 FROM 
-                    branches b
-            )
-            SELECT 
-                customers.client_unique_id,
-                customers.name,
-                customer_metas.tat_days,
-                customer_metas.single_point_of_contact,
-                customers.id AS main_id,
-                COALESCE(branch_counts.branch_count, 0) AS branch_count,
-                COALESCE(application_counts.application_count, 0) AS application_count
-            FROM 
-                customers
-            LEFT JOIN 
-                customer_metas ON customers.id = customer_metas.customer_id
-            LEFT JOIN (
-                SELECT 
-                    customer_id, 
-                    COUNT(*) AS branch_count
-                FROM 
-                    branches
-                GROUP BY 
-                    customer_id
-            ) AS branch_counts ON customers.id = branch_counts.customer_id
-            LEFT JOIN (
-                SELECT 
-                    b.customer_id, 
-                    COUNT(ca.id) AS application_count,
-                    MAX(ca.created_at) AS latest_application_date
-                FROM 
-                    BranchesCTE b
-                INNER JOIN 
-                    client_applications ca ON b.branch_id = ca.branch_id
-                /* WHERE ca.status != 'closed' */
-                GROUP BY 
-                    b.customer_id
-            ) AS application_counts ON customers.id = application_counts.customer_id
-            WHERE 
-                COALESCE(application_counts.application_count, 0) > 0
+                    customers
+                LEFT JOIN 
+                    customer_metas ON customers.id = customer_metas.customer_id
+                LEFT JOIN (
+                    SELECT 
+                        b.customer_id, 
+                        COUNT(*) AS branch_count
+                    FROM 
+                        branches b
+                    WHERE 
+                        EXISTS (
+                            SELECT 1 
+                            FROM candidate_applications ca 
+                            WHERE ca.branch_id = b.id
+                        )
+                    GROUP BY 
+                        b.customer_id
+                ) AS branch_counts ON customers.id = branch_counts.customer_id
+                LEFT JOIN 
+                    ApplicationCounts application_counts ON customers.id = application_counts.customer_id
+                WHERE 
+                    COALESCE(application_counts.application_count, 0) > 0
                 ${customersIDConditionString}
             ORDER BY 
-                application_counts.latest_application_date DESC;
+                    application_counts.latest_application_date DESC;
           `;
 
           connection.query(finalSql, (err, results) => {
