951ee181951adf1b08fe31e286f7000384ed1d30
yashika181007 <yashikawebstep@gmail.com>
2025-01-06 17:08:30 +0530

    updated

--- src/models/admin/callbackModel.js
+++ src/models/admin/callbackModel.js
@@ -4,9 +4,16 @@ const Callback = {
   list: (callback) => {
     const sql = `
       SELECT 
-        *
-      FROM \`callback_requests\`
-      ORDER BY \`requested_at\` DESC
+        cr.*,
+        b.name AS branch_name, 
+        c.name AS customer_name, 
+        c.mobile AS mobile, 
+        cm.single_point_of_contact AS single_point_of_contact 
+      FROM \`callback_requests\` cr
+      INNER JOIN \`branches\` b ON b.id = cr.branch_id
+      INNER JOIN \`customers\` c ON c.id = cr.customer_id
+      INNER JOIN \`customer_metas\` cm ON cm.customer_id = cr.customer_id
+      ORDER BY cr.\`requested_at\` DESC
     `;
 
     startConnection((err, connection) => {
